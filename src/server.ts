6db44b32d305f53f44ee3e55b26eadfe2c2088eb
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-11-22 14:03:56 +0600

    Worked with Eslint, Prettier, Modular System: Routes, Controller, Service, Model

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import mongoose from 'mongoose'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
+    (await mongoose.connect(config.database_url as string))
     app.listen(config, () => {
-      console.log(`Example app listening on port ${config}`)
+      console.log(`App listening on port ${config.port}`)
     })
   } catch (error) {
     console.log(error)
